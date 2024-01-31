b88ac119609c63568a00ad2cb592d4613eaf87e2
satoshi <uglysatoshi@ya.ru>
2024-01-31 19:37:51 +0300

    з2

---
+++ 1.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	var N int
+	_, err := fmt.Scan(&N)
+	if err != nil {
+		return
+	}
+	for i := 0; i < N; i++ {
+		answers := make([]int, 4)
+		boats := make([]int, 10)
+
+		for j := 0; j < len(boats); j++ {
+			_, err2 := fmt.Scan(&boats[j])
+			if err2 != nil {
+				return
+			}
+		}
+
+		for k := 0; k < len(boats); k++ {
+			switch boats[k] {
+			case 1:
+				{
+					answers[0]++
+				}
+			case 2:
+				{
+					answers[1]++
+				}
+			case 3:
+				{
+					answers[2]++
+				}
+			case 4:
+				{
+					answers[3]++
+				}
+			}
+		}
+
+		if answers[0] == 4 && answers[1] == 3 && answers[2] == 2 && answers[3] == 1 {
+			fmt.Println("YES")
+		} else {
+			fmt.Println("NO")
+		}
+
+	}
+}
---
+++ 2.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func main() {
+	var N, d, m, y int
+	_, err := fmt.Scan(&N)
+	if err != nil {
+		return
+	}
+
+	monthValues := make(map[int]int)
+
+	monthValues[1] = 31
+	monthValues[3] = 31
+	monthValues[4] = 30
+	monthValues[5] = 31
+	monthValues[6] = 30
+	monthValues[7] = 31
+	monthValues[8] = 31
+	monthValues[9] = 30
+	monthValues[10] = 31
+	monthValues[11] = 30
+	monthValues[12] = 31
+
+	for i := 0; i < N; i++ {
+		_, err2 := fmt.Scan(&d, &m, &y)
+		if err2 != nil {
+			return
+		}
+		if (1 <= d && d <= 31) && (1 <= m && m <= 12) && (1950 <= y && y <= 2300) {
+			if y%400 == 0 || (y%4 == 0 && y%100 != 0) {
+				monthValues[2] = 29
+				if d <= monthValues[m] {
+					fmt.Println("YES")
+				} else {
+					fmt.Println("NO")
+				}
+			} else {
+				monthValues[2] = 28
+				if d <= monthValues[m] {
+					fmt.Println("YES")
+				} else {
+					fmt.Println("NO")
+				}
+			}
+		} else {
+			fmt.Println("NO")
+		}
+
+	}
+}
